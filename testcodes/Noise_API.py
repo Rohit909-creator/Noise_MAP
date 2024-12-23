0f3ec7902979e37b9f40e5b13d246ee16ced5052
ROHIT FRANCIS <108980398+Rohit909-creator@users.noreply.github.com>
2024-12-23 12:01:50 +0530

    Update Noise_API.py

--- testcodes/Noise_API.py
+++ testcodes/Noise_API.py
@@ -1,7 +1,7 @@
 import requests
 
 req = requests.get("https://api.meersens.com/environment/public/noise/current", headers={
-  'apikey': 'ZHrHb6XRLtGX7peYLG3fWCHUZMDQw2K9'
+  'apikey': 'api_key'
 }, params={
   'lat': 8.484140536292756,
   'lng': 76.94341420944959
